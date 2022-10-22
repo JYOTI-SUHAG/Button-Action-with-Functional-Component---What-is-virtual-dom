3bc4e53d77f3a73ebf15bcb177eadd29a51e543e
JYOTISINGHSUHAG <107070529+JYOTISINGHSUHAG@users.noreply.github.com>
2022-10-22 22:21:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
 import React, {Component, useState} from "react";
+
 import "./../styles/App.css";
 
+ 
+
+ 
+
 function App() {
-  return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
-  );
+
+ const [show, setShow] = useState(false)
+
+ 
+
+ const handleClick = () => {
+
+       // console.log('djgfdj')
+
+       setShow(true)
+
+ }
+
+ return (
+
+   <div id="main">
+
+      <button id="click" type="submit" onClick={()=>{handleClick()}}>Click</button>
+
+      {show === true ? (
+
+       <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+
+       </p>
+
+     ) : undefined }
+
+   </div>
+
+ );
+
 }
 
+ 
+
+ 
 
 export default App;
